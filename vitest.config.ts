c499a7e9a9ffcf7dfd1201d44a68d79e41afb6cb
Chad El Kurdi <73014385+Chadek24295@users.noreply.github.com>
2024-12-02 16:52:58 +0100

    init

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+
+import { defineConfig } from 'vitest/config';
+import solidPlugin from 'vite-plugin-solid';
+
+export default defineConfig({
+    plugins: [solidPlugin()],
+    test: {
+        environment: 'jsdom',
+        globals: true,
+        setupFiles: './vitest.setup.ts',
+        transformMode: {
+        web: [/\.([jt]sx?)$/],
+        },
+    },
+    });
